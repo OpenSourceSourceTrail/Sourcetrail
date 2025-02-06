3343995b245ee053d3fc970db2ba76c1dfd9e627
Muhammad Hussein <32989600+Eng-MohamedHussien@users.noreply.github.com>
2025-02-06 15:51:21 +0200

    chore: migrate dockerfile to ubuntu 24.04 (#26)
    Ticket: SOUR-84

--- scripts/run_conan.sh
+++ scripts/run_conan.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+source ~/env/bin/activate
 conan install . -s build_type=Release -of .conan/gcc   --build=missing -pr:a .conan/gcc/profile
 conan install . -s build_type=Debug   -of .conan/gcc   --build=missing -pr:a .conan/gcc/profile
